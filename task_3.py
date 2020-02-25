991ffc7729c68dd63e06be2b205a4aeaa45544fe
Ruslan <yakovchik.ruslan@gmail.com>
2020-02-25 06:50:31 +0300

    Removed extra functionality

--- task_3.py
+++ task_3.py
@@ -1,20 +1,14 @@
-import time
 from concurrent.futures import ThreadPoolExecutor
 
 
 def function(arg):
     a = 0
-    print("before: " + str(a))
     for _ in range(arg):
         a += 1
-    time.sleep(3)
-    print("after: " + str(a))
     return a
 
 
 def main():
-    start = time.time()
-    a = 0
     max_workers = 5
     arg = [1000000]
 
@@ -22,10 +16,7 @@ def main():
         th_results = executor.map(function, arg*max_workers)
         answer = sum(th_results)
 
-    end = time.time()
-    print(end - start)
-
-    print("----------------------", answer)  # ???
+    print("----------------------", answer)
 
 
 if __name__ == "__main__":
