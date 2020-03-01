b03d0dbb4a6b6786c133c8182631890c480712e7
Ruslan <yakovchik.ruslan@gmail.com>
2020-03-01 11:32:46 +0300

    Changed the logic of threads execution. Created class ModifiableObject.

--- task_3.py
+++ task_3.py
@@ -1,22 +1,45 @@
 from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
 
 
-def function(arg):
-    a = 0
+class ModifiableObject:
+    def __init__(self):
+        self.__a = 0
+        self.__lock = Lock()
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, a):
+        if a:
+            self.__a = a
+        else:
+            print("Incorrect value")
+
+    @property
+    def get_lock(self):
+        return self.__lock
+
+
+def function(arg, my_obj):
     for _ in range(arg):
-        a += 1
-    return a
+        with my_obj.get_lock:
+            my_obj.a += 1
+    return my_obj.a
 
 
 def main():
+
+    my_obj = ModifiableObject()
     max_workers = 5
-    arg = [1000000]
+    value = 1000000
 
     with ThreadPoolExecutor(max_workers) as executor:
-        th_results = executor.map(function, arg*max_workers)
-        answer = sum(th_results)
-
-    print("----------------------", answer)
+        for worker in range(max_workers):
+            executor.submit(function, value, my_obj)
+    print("----------------------", my_obj.a)
 
 
 if __name__ == "__main__":
